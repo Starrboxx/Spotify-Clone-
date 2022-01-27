61d42b743fa877790b46df6d01dcd33d481772b3
Migo Campos <migueljoshuacampos@gmail.com>
2022-01-27 12:57:23 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spotify_Clone',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
